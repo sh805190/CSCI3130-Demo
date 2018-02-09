6fac17652a680e59efc154ea73402071d9d772e3
Star <qwerqazqqq@gmail.com>
2018-02-10 02:11:47 +0800

    Assignment1

--- app/src/main/java/com/example/acme/csci3130_demo/DisplayMessageActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/DisplayMessageActivity.java
@@ -16,7 +16,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
         // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textView2);
+        TextView textView = findViewById(R.id.textView);
         textView.setText(message);
     }
 }
